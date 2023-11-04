ae1b6e948e9d96d1941a78e41706bef604e81f6d
MohamedAboSaleh <abosalehmohamed800@gmail.com>
2023-11-04 16:17:12 +0200

    managing high score using files.

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt",mode="r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -28,6 +29,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.refresh_scoreboard()
 
