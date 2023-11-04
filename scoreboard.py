db9d8fe3ff98e86b6dddb03787469cb50aa6067d
MohamedAboSaleh <abosalehmohamed800@gmail.com>
2023-11-04 14:26:33 +0200

    adding high score to the game.

--- scoreboard.py
+++ scoreboard.py
@@ -1,26 +1,36 @@
 from turtle import Turtle
-ALIGNMENT="center"
-FONT=("Arial", 24, "normal")
+
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
 class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
         self.speed("fastest")
         self.goto(0, 260)
-        self.refresh()
+        self.refresh_scoreboard()
 
-    def refresh(self):
-        self.write(f"score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+    def refresh_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.refresh()
+        self.refresh_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.refresh_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER!", move=False, align=ALIGNMENT, font=FONT)
